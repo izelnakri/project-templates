601a413ba9ff2b224889270ea83b8ca0a0414feb
izelnakri <izelnakri@users.noreply.github.com>
2025-05-13 05:46:36 +0000

    deploy: b09ebb5c4a1bebf36e9fa2faf11c2c9937e8aa5c

--- c/c_09_09_2benchmarks_2bench__user_8cpp.js
+++ c/c_09_09_2benchmarks_2bench__user_8cpp.js
@@ -3,6 +3,6 @@ var c_09_09_2benchmarks_2bench__user_8cpp =
     [ "BENCHMARK", "c_09_09_2benchmarks_2bench__user_8cpp.html#af84149eb4b6ed953b643ab65cadd4c5e", null ],
     [ "BENCHMARK", "c_09_09_2benchmarks_2bench__user_8cpp.html#a37d65130aba284fd1c4190b96560c81b", null ],
     [ "BENCHMARK_MAIN", "c_09_09_2benchmarks_2bench__user_8cpp.html#a5851750faa9cfec10f7cad1f3b89697e", null ],
-    [ "BM_UserConstructor", "c_09_09_2benchmarks_2bench__user_8cpp.html#ad5a5545003942e6788195d3bdce98672", null ],
-    [ "BM_UserPrint", "c_09_09_2benchmarks_2bench__user_8cpp.html#a568e3162859c1f8b496293f2778a45d6", null ]
+    [ "BM_UserConstructor", "c_09_09_2benchmarks_2bench__user_8cpp.html#a466b649d10a75ffa23e814fea092e5d3", null ],
+    [ "BM_UserPrint", "c_09_09_2benchmarks_2bench__user_8cpp.html#af16cd8416df0e014fc6212754a48fc65", null ]
 ];
